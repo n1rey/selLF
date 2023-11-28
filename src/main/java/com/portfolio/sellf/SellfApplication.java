df3b90591c0b3ee313a1c8a2c2940e8ec2f1296e
HanSeungHyeon <tmdgus4720@naver.com>
2023-11-28 16:41:33 +0900

    commit

---
+++ src/main/java/com/portfolio/sellf/SellfApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.sellf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellfApplication.class, args);
+	}
+
+}
