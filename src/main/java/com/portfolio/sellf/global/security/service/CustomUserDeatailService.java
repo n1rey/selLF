20a6ea99056d602fbf2c510382543210cc6e7357
HanSeungHyeon <tmdgus4720@naver.com>
2023-12-05 16:54:05 +0900

    encoder 겹치는거 수정

--- src/main/java/com/portfolio/sellf/global/security/service/CustomUserDeatailService.java
+++ src/main/java/com/portfolio/sellf/global/security/service/CustomUserDeatailService.java
@@ -7,16 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDeatailService implements UserDetailsService {
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-  @Autowired
-  PasswordEncoder passwordEncoder;
 
   @Autowired
   SecurityMapper securityMapper;
