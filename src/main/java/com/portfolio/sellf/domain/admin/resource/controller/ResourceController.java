eef36802533f5e288e511bace4439cd2dc2758b7
HanSeungHyeon <tmdgus4720@naver.com>
2023-12-20 16:45:02 +0900

    리소스화면추가

--- src/main/java/com/portfolio/sellf/domain/admin/resource/controller/ResourceController.java
+++ src/main/java/com/portfolio/sellf/domain/admin/resource/controller/ResourceController.java
@@ -2,15 +2,20 @@ package com.portfolio.sellf.domain.admin.resource.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.portfolio.sellf.domain.admin.resource.service.ResourceService;
 import com.portfolio.sellf.global.common.CommandMap;
 
 @Controller
 @RequestMapping("/admin/resource")
 public class ResourceController {
 
+  @Autowired
+  ResourceService resourceService;
 
     /**
    * <pre>
@@ -21,7 +26,9 @@ public class ResourceController {
    **/
   @RequestMapping(value = {"", "/"}) 
   public String resourcePage(HttpServletRequest request, Model model, CommandMap map) {
-    System.out.println();
+    model.addAttribute("disk", resourceService.getDiskSpace(request));
+    model.addAttribute("cpu", resourceService.getCPUProcess(request));
+    model.addAttribute("memory", resourceService.getMemory());
 
     return "/admin/resource";
   }
--- src/main/java/com/portfolio/sellf/domain/admin/resource/service/ResourceService.java
+++ src/main/java/com/portfolio/sellf/domain/admin/resource/service/ResourceService.java
@@ -1,6 +1,10 @@
 package com.portfolio.sellf.domain.admin.resource.service;
 
 import java.io.File;
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,17 +26,16 @@ public class ResourceService {
   @Autowired
   LogService logService;
 
-    /**
-	 * 디스크 용량
-	 */
+  /** 디스크 용량 **/
   public String[] getDiskSpace(HttpServletRequest request) {
-    String[] diskList = new String[2];
+    String[] diskList = new String[3];
 
     File root = null;
     try {
       root = new File("/");
       diskList[0] = toMB(root.getTotalSpace());
       diskList[1] = toMB(root.getUsableSpace());
+      diskList[2] = String.valueOf(root.getUsableSpace()*100/root.getTotalSpace());
     } catch (Exception e) {
       LogVo logVo = new LogVo();
       logVo.setLogInfo("에러 : "+e.getMessage());
@@ -44,7 +47,37 @@ public class ResourceService {
     return diskList;
   }
 
+  /** cpu 용량 **/
+  public String[] getCPUProcess(HttpServletRequest request) {
+    String[] cpuList = new String[2];
+		OperatingSystemMXBean osbean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();		
+		String cpuUsage = String.format("%.2f", osbean.getSystemCpuLoad() * 100);
 
+		cpuList[0] = cpuUsage;
+    return cpuList;
+  }
+
+  /** 메모리 용량 **/
+  public String[] getMemory() {
+    String[] memList = new String[3];
+    MemoryMXBean membean = (MemoryMXBean) ManagementFactory.getMemoryMXBean();
+		MemoryUsage heap = membean.getHeapMemoryUsage();
+		// MemoryUsage nonheap = membean.getNonHeapMemoryUsage();
+
+		// long heapInit = heap.getInit(); //초기 메모리
+		long heapUsed = heap.getUsed(); //사용 메모리
+		// long heapCommit = heap.getCommitted(); //JVM할당 메모리
+		long heapMax = heap.getMax(); //총 메모리
+		double useMemPct = heapUsed * 100 / heap.getMax(); //메모리 사용량
+
+    memList[0] = String.valueOf(useMemPct);
+    memList[1] = String.valueOf(heapUsed);
+    memList[2] = String.valueOf(heapMax);
+    return memList;
+  }
+
+
+  /** 용량변환 **/
   public String toMB(long size) {
 		return String.valueOf((int) (size / (1024 * 1024)));
 	}
